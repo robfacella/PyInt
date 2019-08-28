63190ef71826bfa10496d021f0e0532af9be84b1
Robert Facella <rob.facella@gmail.com>
2019-08-28 13:42:04 -0400

    Age Calc

--- AgeCalc.py
+++ AgeCalc.py
@@ -5,6 +5,18 @@ def calculate_age(dtob):
     today = date.today()
     return today.year - dtob.year - ((today.month, today.day) < (dtob.month, dtob.day))
 print()
-print(calculate_age(date(2003,12,25)))
-print(calculate_age(date(1996,6,27)))
-print()
+#print(calculate_age(date(2003,12,25)))
+#print(calculate_age(date(1996,6,27)))
+year = int(input('Enter your birth year: '))
+month = int(input('[1-12] Enter your birth month: '))
+day = int(input('[1-31] Enter your birthday: '))
+dob = date(year,month,day)
+print('You are ', calculate_age(dob), ' years old.')
+#print('You were born on ', dob.month, ' / ', dob.day, ' of ', dob.year )
+#print(dob)
+#dates are stored as <yyyy-mm-dd>
+
+#print("I need a way to store and recall dates. Figure out database and python formatting for it..")
+#Thinking MongoDB
+#pip install pymongo
+#from pymongo import MongoClient
