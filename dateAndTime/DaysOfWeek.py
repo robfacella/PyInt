1ce71d95b2819c82e1754b9fa6e2030bb147bc68
Robert Facella <rob.facella@gmail.com>
2019-08-28 18:38:34 -0400

    WeekdayOrWeekend

--- dateAndTime/DaysOfWeek.py
+++ dateAndTime/DaysOfWeek.py
@@ -6,20 +6,19 @@ import datetime
 def isWeekend(theDay):
     if theDay.weekday() > 4 :
         print ('It\'s a  weekend!')
-    elif:
-        theDay.weekday() == 4 :
+    elif theDay.weekday() == 4 :
         print ('Thank Py it\'s Friday!')
     else:
         print ('It\'s a weekday, back to work.')
 
 #For strftime formatting table see:
 #http://www.cplusplus.com/reference/ctime/strftime/
-dayofweek = datetime.date(2019, 6, 28).strftime("%A")
+dayofweek = datetime.date(1996, 6, 17).strftime("%A")
 print(dayofweek)
 # weekday Monday is 0 and Sunday is 6
-print("weekday():", datetime.date(2019, 6, 28).weekday())
+print("weekday():", datetime.date(1996, 6, 17).weekday())
 # isoweekday() Monday is 1 and Sunday is 7
-print("isoweekday()", datetime.date(2019, 6, 28).isoweekday())
+print("isoweekday()", datetime.date(1996, 6, 17).isoweekday())
 print(' ')
 
 dayofweek = datetime.datetime.today().strftime("%A")
