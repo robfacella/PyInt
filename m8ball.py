a8ef66e8d6b7336803dc5beb811989a4330b1c3a
Robert Facella <rob.facella@gmail.com>
2019-08-29 12:36:58 -0400

    An Oldie but a Goodie

---
+++ m8ball.py
@@ -0,0 +1,27 @@
+import random
+
+def getAnswer(answerNumber):
+    if answerNumber == 1:
+        return 'It is certain'
+    elif answerNumber == 2:
+        return 'It is decidedly so'
+    elif answerNumber == 3:
+        return 'Yes'
+    elif answerNumber == 4:
+        return 'Reply hazy try again'
+    elif answerNumber == 5:
+        return 'Ask again later'
+    elif answerNumber == 6:
+        return 'Concentrate and ask again'
+    elif answerNumber == 7:
+        return 'My reply is no'
+    elif answerNumber == 8:
+        return 'Outlook not so good'
+    elif answerNumber == 9:
+        return 'Very doubtful'
+    else:
+        return 'All hail the magic conch!' #should never get hit, but weird memory error catching
+r = random.randint(1, 9)
+#3 positive, negative, and neutral
+fortune = getAnswer(r)
+print(fortune)
