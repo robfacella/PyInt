c9c75ab7d3d4c5487fcd1713e4808d58d5dbe074
Robert Facella <rob.facella@gmail.com>
2019-09-06 13:47:04 -0400

    ToDo Notes

---
+++ TextFileEditing/copypastaWorsening.py
@@ -0,0 +1,8 @@
+
+#Windows && Unix FileSystem Support
+#Read each line of a file
+#split line to strings
+#CharByChar : Substring
+#### Randomly capitalize/lowercase letters
+#### Random 1337-speak
+#Reassemble and Output to a New Output file
