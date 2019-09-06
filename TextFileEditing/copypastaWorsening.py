9c26372a50da3c4f074138f32ba25e221feba1b0
Robert Facella <rob.facella@gmail.com>
2019-09-06 14:08:35 -0400

    Subfile Support Dbg Print

--- TextFileEditing/copypastaWorsening.py
+++ TextFileEditing/copypastaWorsening.py
@@ -1,3 +1,4 @@
+import os
 
 #Windows && Unix FileSystem Support
 #Read each line of a file
@@ -6,3 +7,5 @@
 #### Randomly capitalize/lowercase letters
 #### Random 1337-speak
 #Reassemble and Output to a New Output file
+print (os.path.join('Input', 'in.txt'))
+print(os.path.join('Output', 'out.txt'))
