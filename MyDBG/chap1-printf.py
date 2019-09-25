7d77ad5d5ca3eaa7c1e966111ce5d0b463202acc
Robert Facella <rob.facella@gmail.com>
2019-09-25 15:19:46 -0400

    Linux Varient as comments

--- MyDBG/chap1-printf.py
+++ MyDBG/chap1-printf.py
@@ -1,5 +1,11 @@
 from ctypes import *
 
+#Windows
 msvcrt = cdll.msvcrt
 msg_string = "Hello World!\n"
 msvcrt.wprintf("Testing: %s", msg_string)
+
+#linux
+#libc = cdll("libc.so.6")
+#msg_string = "Hello World!\n"
+#libc.wprintf("Testing: %s", msg_string)
